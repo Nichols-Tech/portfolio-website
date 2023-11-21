0835c219a07ad1910f1b9bf416e04ccebd04dba5
Joshua Samuel Nichols <122940415+Nichols-Tech@users.noreply.github.com>
2023-11-21 15:01:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "Joshua Samuel Nichols",
+  title: "Data Specialist",
+  email: "joshuasnichols@proton.me",
+  gitHub: "https://github.com/Nichols-Tech",
+  instagram: "https://instagram.com/Nichols.Tech",
   linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
