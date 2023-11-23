6bc9eea8ad35d1f9ed05d59aced902c3593bdac3
Joshua Samuel Nichols <122940415+Nichols-Tech@users.noreply.github.com>
2023-11-23 18:36:08 +0100

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a MBA in Data Science student studying at Swiss School of Business and Management. I have a BBA from Lille IAE.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Back-End",
+  "Data Engineering",
+  "Data Analytics",
+  "Cloud Computing",
+  "Database Management",
+  "Data Privacy and Protection",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Data experience I continually look for new and better ways to make information more fun to experience.";
 
 const About = () => {
   return (
