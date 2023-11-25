1f3007785ad35b2b9cdcbd5516e1202a7fbed31c
Joshua Samuel Nichols <122940415+Nichols-Tech@users.noreply.github.com>
2023-11-25 09:22:12 +0100

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/dataworld4.png";
+import image from "../images/dataworld4.jpg";
 
 const imageAltText = "A male human posing in front of a data science themed background.";
 
